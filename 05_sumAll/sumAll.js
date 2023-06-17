79fb86fe8138e696002c6638026fe60ffb950385
DannyMoulier <mr.moulier@gmail.com>
2023-06-17 15:52:47 +0300

    In progress

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+const sumAll = function(num1, num2) {
+    let arrayOfNums = [];
+    let finalSum = 0;
+    for (let num = num1; num < num2; num++) {
+        arrayOfNums.push(num)
+    }
+    arrayOfNums.push(num2)
 
+    for (let num in arrayOfNums) {
+        finalSum += num
+    }
+    
+    return finalSum
+    
 };
 
-// Do not edit below this line
-module.exports = sumAll;
+
+result = sumAll(1, 4);
+
+console.log(result)
